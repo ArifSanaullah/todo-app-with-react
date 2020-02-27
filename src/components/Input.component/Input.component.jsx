4fc7d6597b78d250b1160e476748751b61f90691
Arif Sanaullah <38174227+ArifSanaullah@users.noreply.github.com>
2020-02-28 01:47:01 +0500

    Revert "refactor: Code review changes"

--- src/components/Input.component/Input.component.jsx
+++ src/components/Input.component/Input.component.jsx
@@ -1,24 +1,20 @@
 import React, { Component } from 'react'
 
-// Make this a functional component 
-// It does not need any state 
 export default class ToDoInput extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
     render() {
-        // Use destructuring whenever possible
-        const { textValue, inputType, inputPlace, changeHandler } = this.props
         return (
             <div>
                 <input
-                    value={textValue}
-                    type={inputType}
-                    placeholder={inputPlace}
-                    onChange={changeHandler}
-
-                />
+                    value={this.props.textValue}
+                    type={this.props.inputType}
+                    placeholder={this.props.inputPlace}
+                    onChange={this.props.changeHandler} 
+                    
+                    />
             </div>
         )
     }
