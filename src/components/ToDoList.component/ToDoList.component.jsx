97f3ade5277d0be5c74b901422491cc253958ce6
umairx97 <umair.bajwa@hotmail.com>
2020-02-28 01:32:43 +0500

    refactor: Code review changes

--- src/components/ToDoList.component/ToDoList.component.jsx
+++ src/components/ToDoList.component/ToDoList.component.jsx
@@ -1,22 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import TodoItem from "../ToDoItem.component/ToDoItem.component";
 import style from "./ToDoList.module.css";
 
 export default class ToDoList extends Component {
   render() {
     const { todos } = this.props;
-    if (todos && todos.length) {
+    // Conditional rendering should be done inside render like below
+    // Every LI will be made now not UL on each iteration
       return (
-        <ul className={style.todoList}>
-          {todos.map((item, index) => (
-            <TodoItem
-              todo={item}
-              key={index}
-              deleteHandler={this.props.deleteHandler}
-            />
-          ))}
-        </ul>
+        <Fragment>
+          <ul className={style.todoList}>
+            {todos &&
+              Boolean(todos.length) &&
+              todos.map((item, index) => (
+                <TodoItem
+                  todo={item}
+                  key={index}
+                  deleteHandler={this.props.deleteHandler}
+                />
+              ))}
+          </ul>
+        </Fragment>
       );
-    } else return null;
   }
 }
